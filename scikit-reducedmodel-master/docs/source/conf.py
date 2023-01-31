542fd4bc5b597fa211f8cd754bcc2cd90c9dfabf
francocerino <francocerino@gmail.com>
2023-01-31 10:45:25 -0300

    1st comcommiy

---
+++ scikit-reducedmodel-master/docs/source/conf.py
@@ -0,0 +1,37 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath("../.."))
+
+#import skreducemodel
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'ScikitReduceModel'
+copyright = '2022, Franco Cerino - Agustín Rodríguez-Medrano'
+author = 'Franco Cerino - Agustín Rodríguez-Medrano'
+release = '0.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["sphinx.ext.autodoc"]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+#html_theme = 'sphinx_pdj_theme'
+#html_theme_path = [sphinx_pdj_theme.get_html_theme_path()]
+html_theme = 'sphinx_rtd_theme'
+#html_theme = 'alabaster'
+#html_static_path = ['_static']
